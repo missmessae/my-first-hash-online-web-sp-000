800227a7775bdde97e94725e3d60f1b8f0376c88
Annie Wiegel <annewiegel17@gmail.com>
2021-02-05 23:26:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,5 @@
-def my_hash
+def my_hash(my_hash)
+  my_hash = {dog => "Emerson", name => "Annie"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
